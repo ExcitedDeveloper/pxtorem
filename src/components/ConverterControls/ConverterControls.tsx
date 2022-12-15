29f26efa476532933aa2bb22a9dd72aa31998967
Steve Hunley <shunley2000@yahoo.com>
2022-12-15 10:42:01 -0600

    WIP

--- src/components/ConverterControls/ConverterControls.tsx
+++ src/components/ConverterControls/ConverterControls.tsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react"
 import "./ConverterControls.css"
+import {
+  ConversionDirection,
+  useConverter,
+} from "../../contexts/Converter/Converter.context"
+
+const PIXELS_LABEL = "Pixels"
+const REM_LABEL = "Rem"
 
 const ConverterControls = () => {
-  return <div className='converter-controls'>Converter Controls</div>
+  const { direction, setDirection } = useConverter()
+  const [leftLabel, setLeftLabel] = useState(PIXELS_LABEL)
+  const [rightLabel, setRightLabel] = useState(REM_LABEL)
+
+  useEffect(() => {
+    if (direction === ConversionDirection.PxToRem) {
+      setLeftLabel(PIXELS_LABEL)
+      setRightLabel(REM_LABEL)
+    } else {
+      setLeftLabel(REM_LABEL)
+      setRightLabel(PIXELS_LABEL)
+    }
+  }, [direction])
+
+  const toggleDirection = () => {
+    setDirection(
+      direction === ConversionDirection.PxToRem
+        ? ConversionDirection.RemToPx
+        : ConversionDirection.PxToRem
+    )
+  }
+
+  return (
+    <div className='converter-controls'>
+      <label htmlFor=''>{leftLabel}</label>
+      <button onClick={toggleDirection}>⇄</button>
+      <label htmlFor=''>{rightLabel}</label>
+    </div>
+  )
 }
 
 export default ConverterControls
--- src/components/ConverterTitle/ConverterTitle.tsx
+++ src/components/ConverterTitle/ConverterTitle.tsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react"
 import "./ConverterTitle.css"
+import {
+  ConversionDirection,
+  useConverter,
+} from "../../contexts/Converter/Converter.context"
+
+const PxToRemText = "PX to REM converter"
+const RemToPxText = "REM to PX converter"
 
 const ConverterTitle = () => {
+  const { direction } = useConverter()
+  const [title, setTitle] = useState(PxToRemText)
+
+  useEffect(() => {
+    if (direction === ConversionDirection.PxToRem) {
+      setTitle(PxToRemText)
+    } else {
+      setTitle(RemToPxText)
+    }
+  }, [direction])
+
   return (
     <div className='converter-title'>
       <div>
-        <div>PX to REM converter</div>
+        <div>{title}</div>
       </div>
     </div>
   )
